96adf4b7595b19dbec407d33fb45cc7943dda14c
faolitarna <szymonkramarczyk@gmail.com>
2025-06-25 15:10:56 +0200

    Exercises 1.3-1.5 update.

--- part1/introdemo/src/App.jsx
+++ part1/introdemo/src/App.jsx
@@ -1,31 +1,76 @@
-const Hello = (props) => {
+import { useState } from 'react'
+
+const Display = (props) => {
   return (
-    <div>
-      <p>Hello {props.name}, you are {props.age} years old.</p>
-    </div>
+    <div>{props.counter}</div>
   )
-} 
+}
+
+const Button = (props) => {
+  return (
+    <button onClick={props.onClick}>
+      {props.text}
+    </button>
+  )
+}
 
 const App = () => {
-  console.log('Hello from component.')
-  const a = 10
-  const b = 20
-  const sum = a + b   
-  const age = 33
-  console.log('Sum:', sum)
+  const [ counter, setCounter ] = useState(0)
 
-  
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
+
   return (
     <div>
-      <p>Hello world</p>
-      <br />
-      <Hello  name="George" age={99+1}/>
-      <Hello  name="Daisy" age={age}/>
-      <Hello  />
-      <br />
-      <p>Sum of {a} plus {b} is: {sum}</p>
+      <Display counter={counter}/>
+      <Button
+        onClick={increaseByOne}
+        text='plus'
+      />
+      <Button
+        onClick={setToZero}
+        text='zero'
+      />     
+      <Button
+        onClick={decreaseByOne}
+        text='minus'
+      />           
     </div>
   )
 }
 
+
+
+// const Hello = (props) => {
+//   return (
+//     <div>
+//       <p>Hello {props.name}, you are {props.age} years old.</p>
+//     </div>
+//   )
+// } 
+
+// const App = () => {
+//   console.log('Hello from component.')
+//   const a = 10
+//   const b = 20
+//   const sum = a + b   
+//   const age = 33
+//   console.log('Sum:', sum)
+
+  
+//   return (
+//     <div>
+//       <p>Hello world</p>
+//       <br />
+//       <Hello  name="George" age={99+1}/>
+//       <Hello  name="Daisy" age={age}/>
+//       <Hello  />
+//       <br />
+//       <p>Sum of {a} plus {b} is: {sum}</p>
+//     </div>
+//   )
+// }
+
 export default App
+
--- part1/introdemo/src/main.jsx
+++ part1/introdemo/src/main.jsx
@@ -1,6 +1,5 @@
+import ReactDOM from 'react-dom/client'
 
-import { createRoot } from 'react-dom/client'
+import App from './App'
 
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
