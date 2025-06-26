b146b7e1b6b9bc3824618145b93d7b8cdc30b43b
faolitarna <szymonkramarczyk@gmail.com>
2025-06-26 18:41:03 +0200

    1d - Handling Arrays

--- part1/introdemo/src/App.jsx
+++ part1/introdemo/src/App.jsx
@@ -1,76 +1,37 @@
 import { useState } from 'react'
 
-const Display = (props) => {
-  return (
-    <div>{props.counter}</div>
-  )
-}
+const Display = ({counter}) => <div>{counter}</div>
 
-const Button = (props) => {
-  return (
-    <button onClick={props.onClick}>
-      {props.text}
-    </button>
-  )
-}
+const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
 
 const App = () => {
-  const [ counter, setCounter ] = useState(0)
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
 
-  const increaseByOne = () => setCounter(counter + 1)
-  const decreaseByOne = () => setCounter(counter - 1)
-  const setToZero = () => setCounter(0)
+  const [allClicks, setAll] = useState([])
+
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
 
   return (
     <div>
-      <Display counter={counter}/>
-      <Button
-        onClick={increaseByOne}
-        text='plus'
-      />
-      <Button
-        onClick={setToZero}
-        text='zero'
-      />     
-      <Button
-        onClick={decreaseByOne}
-        text='minus'
-      />           
+      {left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {right}
+
+      <p>{allClicks.join(' ')}</p>
     </div>
   )
 }
 
-
-
-// const Hello = (props) => {
-//   return (
-//     <div>
-//       <p>Hello {props.name}, you are {props.age} years old.</p>
-//     </div>
-//   )
-// } 
-
-// const App = () => {
-//   console.log('Hello from component.')
-//   const a = 10
-//   const b = 20
-//   const sum = a + b   
-//   const age = 33
-//   console.log('Sum:', sum)
-
-  
-//   return (
-//     <div>
-//       <p>Hello world</p>
-//       <br />
-//       <Hello  name="George" age={99+1}/>
-//       <Hello  name="Daisy" age={age}/>
-//       <Hello  />
-//       <br />
-//       <p>Sum of {a} plus {b} is: {sum}</p>
-//     </div>
-//   )
-// }
-
 export default App
-
